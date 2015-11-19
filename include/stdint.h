e528b47d64cb7b50ffb2f720b4ff812f23865e48
Takahiro Shinagawa <shina@ecc.u-tokyo.ac.jp>
2015-11-19 19:01:03 +0900

    add some C99 interger types to avoid a compile error

--- include/stdint.h
+++ include/stdint.h
@@ -28,3 +28,11 @@
  */
 
 typedef long int intptr_t;
+typedef unsigned long long uint64_t;
+typedef unsigned int       uint32_t;
+typedef unsigned short     uint16_t;
+typedef unsigned char      uint8_t;
+typedef long long          int64_t;
+typedef int                int32_t;
+typedef short              int16_t;
+typedef char               int8_t;
