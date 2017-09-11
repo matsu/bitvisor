396dc1e7290c4df7ca84bf27eed3d46a292da58f
Hideki EIRAKU <hdk@igel.co.jp>
2017-09-11 12:29:43 +0900

    pro1000: add 8086:15d8 I219-V

--- drivers/net/pro1000.c
+++ drivers/net/pro1000.c
@@ -1837,7 +1837,8 @@ static struct pci_driver pro1000_driver = {
 			  "8086:1f41|"
 			  "8086:1f45|"
 			  "8086:15b7|"
- 			  "8086:1570",
+ 			  "8086:1570|"
+ 			  "8086:15d8",
 	.new		= pro1000_new,	
 	.config_read	= pro1000_config_read,
 	.config_write	= pro1000_config_write,
