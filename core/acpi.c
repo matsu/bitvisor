c535eef3c52e0e6f82658d315b3044683bcada34
Ake Koomsin <ake@igel.co.jp>
2023-05-17 17:47:25 +0900

    acpi: move IO hook related functions to acpi_iohook.c
    Note that acpi_pm1_sleep() is in acpi_iohook.c to simplify refactoring
    other architecture support. This makes acpi.c independent from x86
    specific implementation.

--- core/acpi.c
+++ core/acpi.c
@@ -37,15 +37,12 @@
 #include "constants.h"
 #include "current.h"
 #include "initfunc.h"
-#include "io_io.h"
 #include "mm.h"
 #include "panic.h"
 #include "printf.h"
 #include "sleep.h"
 #include "string.h"
-#include "time.h"
 #include "uefi.h"
-#include "wakeup.h"
 
 #define FIND_RSDP_NOT_FOUND	0xFFFFFFFFFFFFFFFFULL
 #define RSDP_SIGNATURE		"RSD PTR"
@@ -631,101 +628,6 @@ debug_dump_reg (struct acpi_reg *r)
 }
 
 static bool
-acpi_pm1_sleep (u32 v)
-{
-	u32 new_waking_vector;
-	u32 old_waking_vector;
-	bool error;
-
-	if (!acpi_dsdt_pm1_cnt_slp_typx_check (v))
-		return false;
-	ASSERT (acpi_get_waking_vector (&old_waking_vector, &error));
-	if (error) /* In case of FACS error */
-		return true;
-	new_waking_vector = prepare_for_sleep (old_waking_vector);
-	acpi_set_waking_vector (new_waking_vector, old_waking_vector);
-	get_cpu_time ();	/* update lastcputime */
-	/* Flush all write back caches including the internal caches
-	   on the other processors, or the processors will lose them
-	   and the VMM will not work correctly. */
-	mm_flush_wb_cache ();
-	acpi_reg_pm1a_cnt_write (v);
-	cancel_sleep ();
-	return true;
-}
-
-static enum ioact
-acpi_io_monitor (enum iotype type, u32 port, void *data)
-{
-	u32 v;
-	u32 pm1a_cnt_ioaddr;
-
-	if (acpi_reg_get_pm1a_cnt_ioaddr (&pm1a_cnt_ioaddr) &&
-	    port == pm1a_cnt_ioaddr) {
-		switch (type) {
-		case IOTYPE_OUTB:
-			v = *(u8 *)data;
-			break;
-		case IOTYPE_OUTW:
-			v = *(u16 *)data;
-			break;
-		case IOTYPE_OUTL:
-			v = *(u32 *)data;
-			break;
-		default:
-			goto def;
-		}
-		if (v & ACPI_PM1_CNT_SLP_EN_BIT)
-			if (acpi_pm1_sleep (v))
-				return IOACT_CONT;
-		goto def;
-	}
-def:
-	return do_io_default (type, port, data);
-}
-
-static enum ioact
-acpi_smi_monitor (enum iotype type, u32 port, void *data)
-{
-	u32 smi_cmd;
-
-	if (current->acpi.smi_hook_disabled)
-		panic ("SMI monitor called while SMI hook is disabled");
-	current->vmctl.paging_map_1mb ();
-	ASSERT (acpi_reg_get_smi_cmd_ioaddr (&smi_cmd));
-	current->vmctl.iopass (smi_cmd, true);
-	current->acpi.smi_hook_disabled = true;
-	return IOACT_RERUN;
-}
-
-void
-acpi_smi_hook (void)
-{
-	u32 smi_cmd;
-
-	if (!current->vcpu0->acpi.iopass)
-		return;
-	if (current->acpi.smi_hook_disabled) {
-		ASSERT (acpi_reg_get_smi_cmd_ioaddr (&smi_cmd));
-		current->vmctl.iopass (smi_cmd, false);
-		current->acpi.smi_hook_disabled = false;
-	}
-}
-
-static void
-acpi_iohook (void)
-{
-	u32 pm1a_cnt_ioaddr, smi_cmd;
-
-	if (acpi_reg_get_pm1a_cnt_ioaddr (&pm1a_cnt_ioaddr))
-		set_iofunc (pm1a_cnt_ioaddr, acpi_io_monitor);
-	if (acpi_reg_get_smi_cmd_ioaddr (&smi_cmd)) {
-		current->vcpu0->acpi.iopass = true;
-		set_iofunc (smi_cmd, acpi_smi_monitor);
-	}
-}
-
-static bool
 ioaddr_is_valid (u32 ioaddr)
 {
 	return ioaddr <= 0xFFFF;
@@ -1306,4 +1208,3 @@ acpi_init_global (void)
 
 INITFUNC ("global3", acpi_init_global);
 INITFUNC ("paral30", acpi_init_paral);
-INITFUNC ("iohook1", acpi_iohook);
